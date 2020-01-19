740cdc2f2a1d772c9c9fabcc9ce7b0a53fa58d97
Matthew Zendejas <user@matthewzendejas@hotmail.com>
2020-01-18 20:19:10 -0500

    halfway done

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
@@ -1,9 +1,39 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
-
+		
+		try (Scanner scanner = new Scanner(System.in)) {
+		boolean linearConversion = true;
+		while(linearConversion) {
+			System.out.println ("Congratulations on Completing your exercize for the day!");
+			System.out.println ("Please enter the distance you traveled today :");
+			String value = scanner.nextLine();
+		
+			 //how to put "if value != int//
+//				System.out.println("please try again!");
+//			}
+			
+			float distanceToConvert = Float.parseFloat(value);
+			System.out.println("Please enter 'm' for meters or 'f' for feet: ");
+			String distanceType = scanner.nextLine();
+			
+			
+			if (distanceType.equalsIgnoreCase("m")) {
+				float convertedDistance = distanceToConvert * (float) 3.2808399;
+				System.out.println("your distance is " + distanceToConvert + " in meters" + "and " + convertedDistance + " in feet!");
+			break; }
+			else if (distanceType.equalsIgnoreCase("f")) {
+				float convertedDistance = distanceToConvert * (float) 0.3048;
+				System.out.println("your distance is " + distanceToConvert + "in feet" + "and " + convertedDistance + " in meters!");
+			break; }
+			else 
+				System.out.println("Please try again");
+		}
+		}
 	}
-
 }
+
--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
@@ -1,9 +1,32 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
-
+try (Scanner scanner = new Scanner (System.in)) {
+	boolean temperatureConversion = true;
+	while (temperatureConversion) {
+		System.out.println("Hello, please type any temperature");
+		String value = scanner.nextLine();
+		
+		float temperatureInput = Float.parseFloat(value);
+		
+		System.out.println("Type 'f' for Fahrenheit or 'c' for Celcius");
+		String typeToConvert = scanner.nextLine();
+		if (typeToConvert.equalsIgnoreCase("f")) {
+			float convertedTemperature = (temperatureInput - 32) / (float)1.8;
+		System.out.println(temperatureInput + " degrees Celcius is " + convertedTemperature + " Fahrenheit" );
+			break; }
+	else if (typeToConvert.equalsIgnoreCase("c")) {
+		float convertedTemperature = (float) temperatureInput * (float) 1.8 + 32 ;
+		System.out.println(temperatureInput + " degrees Fahrenheit is " + convertedTemperature + " Celcius");
+			break;}
+	else {
+	System.out.println("Error Jordan; please try again");
+}
 	}
-
+}
+	}
 }
