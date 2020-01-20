23e596fdfb02935150fb355982da3afc015eceb0
Matthew Zendejas <user@matthewzendejas@hotmail.com>
2020-01-20 07:45:04 -0500

    final

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
@@ -17,11 +17,11 @@ try (Scanner scanner = new Scanner (System.in)) {
 		String typeToConvert = scanner.nextLine();
 		if (typeToConvert.equalsIgnoreCase("f")) {
 			float convertedTemperature = (temperatureInput - 32) / (float)1.8;
-		System.out.println(temperatureInput + " degrees Celcius is " + convertedTemperature + " Fahrenheit" );
+		System.out.println(temperatureInput + " degrees Fahrenheit is " + convertedTemperature + " Celcius" );
 			break; }
 	else if (typeToConvert.equalsIgnoreCase("c")) {
 		float convertedTemperature = (float) temperatureInput * (float) 1.8 + 32 ;
-		System.out.println(temperatureInput + " degrees Fahrenheit is " + convertedTemperature + " Celcius");
+		System.out.println(temperatureInput + " degrees Celcius is " + convertedTemperature + " Fahrenheit");
 			break;}
 	else {
 	System.out.println("Error Jordan; please try again");
