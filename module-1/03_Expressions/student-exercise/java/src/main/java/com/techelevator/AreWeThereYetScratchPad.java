84e7bdf9d75f208c69a53a0ffbf89eb9815db695
Matthew Zendejas <user@matthewzendejas@hotmail.com>
2020-01-21 12:01:34 -0500

    commented out errors

--- module-1/03_Expressions/student-exercise/java/src/main/java/com/techelevator/AreWeThereYetScratchPad.java
+++ module-1/03_Expressions/student-exercise/java/src/main/java/com/techelevator/AreWeThereYetScratchPad.java
@@ -1,27 +1,27 @@
-package com.techelevator;
-
-import java.util.Scanner;
-
-public class AreWeThereYetScratchPad {
-
-	while (!there) {
-		System.out.println("Bart: Are we there yet? (Enter 'Y' or 'N')");
-		
-		String answer = in.nextLine();
-		
-		 if (answer.equals("Y") || (answer.equals("y"))) {
-
-		System.out.println("Yippie!!!");
-		break;
-	}
-		 else {
-				System.out.println("BOOO!!!!");
-
-	}
-	System.out.println("The value of answer: " + answer);
-}
-}
-}
-
-	}
-	
+//package com.techelevator;
+//
+//import java.util.Scanner;
+//
+//public class AreWeThereYetScratchPad {
+//
+//	while (!there) {
+//		System.out.println("Bart: Are we there yet? (Enter 'Y' or 'N')");
+//		
+//		String answer = in.nextLine();
+//		
+//		 if (answer.equals("Y") || (answer.equals("y"))) {
+//
+//		System.out.println("Yippie!!!");
+//		break;
+//	}
+//		 else {
+//				System.out.println("BOOO!!!!");
+//
+//	}
+//	System.out.println("The value of answer: " + answer);
+//}
+//}
+//}
+//
+//	}
+//	
