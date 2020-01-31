851b361e5ef8afe6043e35209fb1ee60185406fb
Matthew Zendejas <user@matthewzendejas@hotmail.com>
2020-01-31 17:49:27 -0500

    Add test method testDivisibleBy3 'untested'

--- module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -2,4 +2,10 @@ package com.techelevator;
 
 public class KataFizzBuzz {
 
+	public String Fizzbuzz (int n) {
+		return null;
+		
+	}
+	
+	
 }
--- module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -3,6 +3,20 @@ package com.techelevator;
 import org.junit.*;
 import static org.junit.Assert.*;
 
-public class KataFizzBuzzTest {
+	
 
+public class KataFizzBuzzTest {
+	
+	KataFizzBuzz fizzybuzzy = new KataFizzBuzz();
+	
+	@Before
+	public void testStart() {
+		
+	}
+	
+	@Test
+	public void testDivisibleBy3() {
+	Assert.assertEquals("Should test 'Fizz'", "Fizz", fizzybuzzy.Fizzbuzz((3)));
+
+}
 }
