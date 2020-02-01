ef76f8fca1d9fdb3514033a9fd151abc20ab0bb0
Matthew Zendejas <user@matthewzendejas@hotmail.com>
2020-02-01 14:09:39 -0500

    add passing code for test method result should be converted to string

--- module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -3,19 +3,19 @@ package com.techelevator;
 public class KataFizzBuzz {
 
 	public String Fizzbuzz (int n) {
-		if(n!=0 && n%5==0 && n%3 ==0) {
+		String a = String.valueOf(n);
+		if (n == 0 || n%5 != 0 && n%3 != 0) {
+		return a;
+		}
+		else if(n!=0 && n%5==0 && n%3 ==0) {
 			return "FizzBuzz";
 		}
 		else if(n!=0 && n%3==0) {
 			return "Fizz";
 		}
-		else if(n!=0 && n%5==0) {
+		else if(n!=0 && n%5==0) {	
 			return "Buzz";
 		}
-		else 
 		return null;
-		
-	}
-	
-	
+}
 }
--- module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -16,13 +16,11 @@ public class KataFizzBuzzTest {
 	
 	@Test
 	public void testDivisibleBy3() {
-	Assert.assertEquals("Should test null", null, fizzybuzzy.Fizzbuzz(0));
 	Assert.assertEquals("should test Fizz", "Fizz", fizzybuzzy.Fizzbuzz(33));
 	Assert.assertEquals("should test Fizz", "Fizz", fizzybuzzy.Fizzbuzz(3));
 }
 	@Test
 	public void testDivisibleBy5() {
-		Assert.assertEquals("Should test buzz", null, fizzybuzzy.Fizzbuzz(0));
 		Assert.assertEquals("Should test buzz", "Buzz", fizzybuzzy.Fizzbuzz(5));
 		Assert.assertEquals("Should test buzz", "Buzz", fizzybuzzy.Fizzbuzz(100));
 
@@ -34,13 +32,14 @@ public class KataFizzBuzzTest {
 		Assert.assertEquals("Should test fizzbuzz", "FizzBuzz", fizzybuzzy.Fizzbuzz(45));
 
 	}
+	@Test
 	public void testNotDivisibleBy5or3() {
 		Assert.assertEquals("Should test convert", "11", fizzybuzzy.Fizzbuzz(11));
 		Assert.assertEquals("Should test convert", "1", fizzybuzzy.Fizzbuzz(1));
-		Assert.assertEquals("Should test convert", "99", fizzybuzzy.Fizzbuzz(99));
+		Assert.assertEquals("Should test convert", "98", fizzybuzzy.Fizzbuzz(98));
 
 }
-
+}
 
 //Add test method, result should be X when X"
 //"add passing code for test method" result_should_be_fizz_when_number_3
