92a87a75982b24177b7595cc2de72922abc0655c
Matthew Zendejas <user@matthewzendejas@hotmail.com>
2020-02-01 13:29:55 -0500

    passing code for test method result_should_be_fizz_with_3

--- module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -3,6 +3,10 @@ package com.techelevator;
 public class KataFizzBuzz {
 
 	public String Fizzbuzz (int n) {
+		if(n!=0 && n%3==0) {
+			return "Fizz";
+		}
+		else 
 		return null;
 		
 	}
--- module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -16,7 +16,11 @@ public class KataFizzBuzzTest {
 	
 	@Test
 	public void testDivisibleBy3() {
-	Assert.assertEquals("Should test null", null, fizzybuzzy.Fizzbuzz(3));
+	Assert.assertEquals("Should test null", null, fizzybuzzy.Fizzbuzz(0));
+	Assert.assertEquals("should test Fizz", "Fizz", fizzybuzzy.Fizzbuzz(3));
+}
+}
 
-}
-}
+
+
+//once test is passing add and commit the code with the message "add passing code for test method" result_should_be_fizz_when_number_3
